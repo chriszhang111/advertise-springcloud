b4b7063f1a6868d36d4ceea8e94a5da0b4c2f22b
chris <869415122@qq.com>
2019-10-06 20:05:59 -0400

    zuul gateway app added

---
+++ ad-gateway/src/main/java/com/chris/ad/ZuulGateWayApplication.java
@@ -0,0 +1,14 @@
+package com.chris.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGateWayApplication.class, args);
+    }
+}
