223923b7e15a01db29c91305b1a97d43ab64971d
chris <869415122@qq.com>
2019-11-03 21:12:00 -0500

    add service in controller

--- chris-ad-service/ad-search/src/main/java/com/chris/ad/controller/SearchController.java
+++ chris-ad-service/ad-search/src/main/java/com/chris/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.chris.ad.annotation.IgnoreResponseAdvice;
 import com.chris.ad.client.SponsorClient;
 import com.chris.ad.client.vo.AdPlan;
 import com.chris.ad.client.vo.AdPlanGetRequest;
+import com.chris.ad.search.ISearch;
+import com.chris.ad.search.vo.SearchRequest;
+import com.chris.ad.search.vo.SearchResponse;
 import com.chris.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +24,13 @@ public class SearchController {
 
     private RestTemplate restTemplate;
     private SponsorClient sponsorClient;
+    private ISearch search;
 
-    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient) {
+    @Autowired
+    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient, ISearch search) {
         this.restTemplate = restTemplate;
         this.sponsorClient = sponsorClient;
+        this.search = search;
     }
 
     @IgnoreResponseAdvice
@@ -46,5 +52,11 @@ public class SearchController {
     public CommonResponse<List<AdPlan>> getAdPlans(@RequestBody AdPlanGetRequest request){
         log.info("ad-search: getAdPlans -> {}", JSON.toJSONString(request));
         return sponsorClient.getAdPlans(request);
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request){
+        log.info("ad-search:fecthAds -> {}", JSON.toJSONString(request));
+        return search.fetchAds(request);
     }
 }
