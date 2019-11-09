9ecc3f92895636dfb0bb42c4c9d4f9d5687a293f
chris <869415122@qq.com>
2019-11-09 14:37:01 -0500

    import mysql data fixed

--- chris-ad-service/ad-common/src/main/java/com/chris/ad/dump/DConstant.java
+++ chris-ad-service/ad-common/src/main/java/com/chris/ad/dump/DConstant.java
@@ -2,12 +2,12 @@ package com.chris.ad.dump;
 
 public class DConstant {
 
-    public static final String DATA_ROOT_DIR = "/Users/chris/Desktop/";
+    public static final String DATA_ROOT_DIR = "/Users/chris/Desktop/spring/mysql_data/";
     public static final String AD_PLAN = "ad_plan.txt";
-    public static final String AD_UNIT = "ad_unit.data";
-    public static final String AD_CREATIVE = "ad_creative.data";
-    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
-    public static final String AD_UNIT_IT = "ad_unit_it.data";
-    public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
-    public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
+    public static final String AD_UNIT = "ad_unit.txt";
+    public static final String AD_CREATIVE = "ad_creative.txt";
+    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.txt";
+    public static final String AD_UNIT_IT = "ad_unit_it.txt";
+    public static final String AD_UNIT_DISTRICT = "ad_unit_district.txt";
+    public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.txt";
 }
